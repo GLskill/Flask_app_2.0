c3b74cdc8d89dc2a914fc20a0f59b98515cd9e25
GLskill <Glebluz1488@gmail.com>
2024-09-26 16:44:24 +0200

    adding a compiler  SCSS in CSS

---
+++ compile.py
@@ -0,0 +1,15 @@
+import sass
+import os
+
+# Path to styles.scss
+scss_file = 'styles.scss'
+css_file = os.path.join('static', 'styles.css')
+
+# Compile to SCSS in CSS
+css = sass.compile(filename=scss_file)
+
+# save result
+with open(css_file, 'w') as f:
+    f.write(css)
+
+print("Compile is ready")
